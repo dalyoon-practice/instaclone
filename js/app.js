a543210e2f8a476dcaeda483ba570fe91c6765c1
DalYoon <yeodal89@gmail.com>
2017-07-09 16:22:27 +0900

    add_comment_done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-	var	$heart = $('.heart');
+
+	var	$heart = $('.heart'),
+		$addComment = $('.photo__add-comment');
 
 	$heart.click(function() {
 		$(this).toggleClass('fa-heart-o fa-heart');
+	})
+
+	$addComment.keydown(function(event){
+		if(event.keyCode == 13){
+//			console.log('pressed enter', event.target.value)
+			var newComment = event.target.value;
+//			console.log($(this).parent())
+			var commentList = $(this).parent().parent().children('.photo__comments')
+//			commentList.append('<p>hello</p>')
+			$(this).val('').blur();
+
+			commentList.append("<li class='photo__comment'>\
+				<span class='photo__comment-author'>Visitor</span> \
+				"+ newComment +"</li>")
+		}
 	})
 })
